c9d956a3f045fac848684940a03fa65710af1a23
kevin <mrbinye@gmail.com>
2022-04-25 18:37:04 +0100

    update script

--- RPS_game.py
+++ RPS_game.py
@@ -1,40 +1,33 @@
 import cv2
 from keras.models import load_model
 import numpy as np
-from game_logic import play_RPS,make_predication,countdown
+from game_logic import imshow_with_text, play_RPS,make_predication
 import time
 
-TIMER = int(5)
+
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0) ##Open the first camera for video capturing.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32) ## 4D array, image size and RGB values
-
-cp_win_count = 0 
-usr_win_count = 0
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32) 
+## 4D array, image size and RGB values
+ 
+TIMER = int(5)   #countdown
+cp_win_count = 0  # victories of computer
+usr_win_count = 0 # victories of user
 
 while True: 
 
     if cp_win_count == 0 and usr_win_count == 0:
-        ret, frame = cap.read() ##ret indicates if the read was successful, frmae is image itself
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame,"Press S to start !",
-                            (150,250),font,
-                            1.5,(0,255,255),
-                            2,cv2.LINE_AA)
-        cv2.imshow('frame', frame)
+ 
+        imshow_with_text(cap,"Press S to start",(150,250),1.5,2,usr_win_count,cp_win_count)
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        prev = time.time() #
+        prev = time.time() 
         while TIMER > 0:
-            ret, frame = cap.read()
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame,str(TIMER),
-                        (200,250),font,
-                        7,(0,255,255),
-                        4,cv2.LINE_AA)
-            cv2.imshow('frame',frame)
+
+            imshow_with_text(cap,str(TIMER),(200,250),7,4,usr_win_count,cp_win_count)
             cv2.waitKey(125)
 
+            ##countdown
             cur = time.time()
             if cur - prev >= 1:
                 prev = cur
@@ -42,35 +35,26 @@ while True:
         else:
             ret,frame = cap.read()
             cv2.imshow('frame',frame)
-            cv2.waitKey(1000)
+            # cv2.imwrite('images/frame_'+str(cur)+".jpg",frame)
+            cv2.waitKey(500)
             usr_index = make_predication(frame,data,model)
             usr_win_count,cp_win_count = play_RPS(usr_index,usr_win_count,cp_win_count)
 
+    #Press c to continue
     if usr_win_count == 3 or cp_win_count == 3:
-        if usr_win_count > cp_win_count:    
-            print("You defeated the computer !!!")
-        else:
-            print("The computer defeated you !!!")
 
-   
-        ret, frame = cap.read()
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame,"Press C to continue !",
-                        (150,250),font,
-                        1.5,(0,255,255),
-                        2,cv2.LINE_AA)
-        cv2.imshow('frame', frame)
+        imshow_with_text(cap,"Press C to continue !",(120,250),1.5,2,usr_win_count,cp_win_count)
         if cv2.waitKey(1) & 0xFF == ord('c'):
             usr_win_count = 0
             cp_win_count = 0
-        elif cv2.waitKey(1) & 0xFF == ord('q'): # Press q to close the window
-            break
 
+    # Press q to close the window
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
     TIMER = int(5)
-
-
 
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+
--- game_logic.py
+++ game_logic.py
@@ -1,65 +1,90 @@
 import imp
 import random
 import cv2
+from cv2 import imshow
 from keras.models import load_model
 import numpy as np
-import time
 
+#Figure out who win
 def play_RPS(index_usr,usr_win_count,cp_win_count):
-    
     cp_poses = ['Rock','Paper','Scissors']
     poses = ['Rock','Paper','Scissors','Nothing']
 
-    cp_pose = random.choice(cp_poses)
-    
     # index_usr = input('0.Rock 1.Paper 2.Scissors  3.Nothing')
     usr_pose = poses[index_usr]
-
+    cp_pose = random.choice(cp_poses)
     print('Computer chose ',cp_pose)
     print('User chose ',usr_pose)
 
-    if cp_pose == usr_pose:
+    if usr_pose == cp_pose:
         print(f"You both chose {usr_pose}. It's a tie !",)
-    elif usr_pose == "Rock":
-        if cp_pose == "Paper":
-            cp_win_count = cp_win_count +1
-            print("You lose ...")
-        else:
-            usr_win_count = usr_win_count +1
-            print("You win !!!")
-    elif usr_pose == "Paper":
-        if cp_pose == "Scissors":
-            cp_win_count  = cp_win_count  +1
-            print("You lose ...")
-        else:
-            usr_win_count = usr_win_count +1
-            print("You win !!!")
-    elif usr_pose == "Scissors":
-        if cp_pose == "Rock":
-            cp_win_count = cp_win_count +1
-            print("You lose ...")
-        else:
-            usr_win_count = usr_win_count +1
-            print("You win !!!")
-    else:
+    elif usr_pose == "Nothing":
         print("Please get ready for your action")
+    elif usr_pose == "Rock" and cp_pose == "Scissors":
+        usr_win_count = usr_win_count +1
+        print("You win !!!")
+    elif usr_pose == "Paper" and cp_pose == "Rock":
+        usr_win_count = usr_win_count +1
+        print("You win !!!")
+    elif usr_pose == "Scissors" and cp_pose == "Paper":
+        usr_win_count = usr_win_count +1
+        print("You win !!!")
+    else:
+        cp_win_count = cp_win_count + 1
+        print("You lose !!!")
     print()
+    ## decide who is winner
+    if usr_win_count == 3 or cp_win_count == 3:
+        if usr_win_count > cp_win_count:    
+            print("You are the winner !!!")
+        else:
+            print("The computer is the winner !!!")
     return usr_win_count,cp_win_count
 
+#Make prediction from the image
 def make_predication(frame,data,model):
-    height, width, channels = frame.shape
+    #image[y:y+h,x:x+h]
     crop_frame = frame[:,0:480]
+    # cv2.imwrite("images/1.jpg",crop_frame)
     resized_frame = cv2.resize(crop_frame, (224, 224), interpolation = cv2.INTER_AREA) ## resampling using pixel area relation.
     image_np = np.array(resized_frame) ## get the image array
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image [0,255] to [0,1]
     data[0] = normalized_image
     prediction = model.predict(data)
-    if prediction[0][0] > 0.5:
-        usr_index = 0
-    elif prediction[0][1] > 0.5:
-        usr_index = 1
-    elif prediction[0][2] > 0.5:
-        usr_index = 2
-    else:
-        usr_index = 3
+    # list_prediction = list(prediction[0])
+    #use list.index() to find the maximun
+
+    # if prediction[0][0] > 0.5:
+    #     usr_index = 0
+    # elif prediction[0][1] > 0.5:
+    #     usr_index = 1
+    # elif prediction[0][2] > 0.5:
+    #     usr_index = 2
+    # else:
+    #     usr_index = 3
+    # print("Rock:",prediction[0][0])
+    # print("Paper:",prediction[0][1])
+    # print("Scissors:",prediction[0][2])
+    # print("Nothing:",prediction[0][3])
+
+    list_prediction = list(prediction[0])
+    usr_index = list_prediction.index(max(list_prediction))
+
     return usr_index
+
+def imshow_with_text(cap,text,text_positon,fontsize,thickness,usr_win_count,cp_win_count):
+    ret, frame = cap.read()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame,text,
+                    text_positon,font,
+                    fontsize,(0,255,255),
+                    thickness,cv2.LINE_AA)
+    cv2.putText(frame,"usr:"+str(usr_win_count),
+                    (50,20),font,
+                    0.8,(0,255,255),
+                    2,cv2.LINE_4)
+    cv2.putText(frame,"computer:"+str(cp_win_count),
+                    (450,20),font,
+                    0.8,(0,255,255),
+                    2,cv2.LINE_4)
+    cv2.imshow('frame', frame)
